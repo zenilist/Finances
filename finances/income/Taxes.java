fe2427cd7b2cda0994e99ee87d84601ccaf3c30e
zenilist <aviraj.kar2012@gmail.com>
2024-01-20 19:01:00 -0500

    Integrate Maven build system:
    - Added Maven configuration files (pom.xml).
    - Configured dependencies, including OpenCSV.
    - Utilized maven-jar-plugin to set the main class in the JAR manifest.
    - Implemented maven-shade-plugin for creating an executable JAR with dependencies.
    - Updated project structure to align with Maven conventions.
    - Streamlined build and execution process.
    - Cleaned up unnecessary files and configurations.

--- finances/income/Taxes.java
+++
@@ -1,7 +0,0 @@
-package finances.income;
-
-public interface Taxes {
-    double calculateTax(double taxableWages);
-
-    double getTaxAmount();
-}
--- finances/income/TspRetirement.java
+++
@@ -1,9 +0,0 @@
-package finances.income;
-
-public interface TspRetirement {
-    double calculateDeductions(double GrossPay, double retirementContributionRate);
-
-    double getTotalContributions();
-
-    double getContribution();
-}
